a53f599e3e7de5e989397bf3829e5f8b7548cc35
Shahar Evron <shahar@shoppimon.com>
2018-12-09 17:31:27 +0200

    Change system tests to work using forks and not threads
    This improves testing stability and makes system tests more “real world” like.

---
+++ tests/system/conftest.py
@@ -0,0 +1,88 @@
+"""Testing Configuration for System Tests
+"""
+import logging
+import os
+import sys
+from contextlib import contextmanager
+from importlib import reload
+from multiprocessing import Process
+from subprocess import Popen
+from tempfile import mkstemp
+
+import pytest
+import zmq
+
+from majortomo import Worker, WorkerRequestsIterator
+from majortomo.config import default_config
+
+HEARTBEAT_INTERVAL = 1.0
+HEARTBEAT_TIMEOUT = 3.0
+
+
+@pytest.fixture()
+def socket_url():
+    fh, path = mkstemp(prefix='majortomo-test-')
+    os.close(fh)
+    try:
+        yield 'ipc://' + path
+    finally:
+        os.unlink(path)
+
+
+@contextmanager
+def run_broker(bind):
+    """Run Broker in a thread, with context manager
+    """
+    command = [sys.executable, '-m', 'majortomo.broker',
+               '-b', bind,
+               '-i', str(HEARTBEAT_INTERVAL),
+               '-t', str(HEARTBEAT_TIMEOUT),
+               '--debug']
+
+    broker_proc = Popen(command)
+
+    try:
+        yield
+    finally:
+        broker_proc.terminate()
+        broker_proc.wait(timeout=3.0)
+
+
+@contextmanager
+def run_worker(connect, service_name='my-service', handler_func=lambda r: r, send_partials=False):
+
+    def _worker_process():
+        # Reset logging
+        logging.shutdown()
+        reload(logging)
+        logging.basicConfig(level=logging.DEBUG, format=default_config['logging']['formatters']['default']['format'])
+
+        # Reset ZMQ context
+        zmq.Context.instance().destroy()
+
+        logging.debug("Starting worker process for service %s", service_name)
+        worker = Worker(broker_url=connect, service_name=service_name, heartbeat_interval=HEARTBEAT_INTERVAL,
+                        heartbeat_timeout=HEARTBEAT_TIMEOUT, zmq_linger=500)
+        worker_iter = WorkerRequestsIterator(worker)
+        worker_iter.stop_on_signal()
+
+        for request in worker_iter:
+                response = list(handler_func(request))
+                if send_partials and len(response) > 1:
+                    partials = response[0:-1]
+                    final = response[-1]
+                    worker_iter.send_reply_from_iterable(partials, final=final)
+                else:
+                    worker_iter.send_reply_final(response)
+
+        logging.debug("Stopping worker process for service %s", service_name)
+
+    proc = Process(target=_worker_process)
+    try:
+        proc.start()
+        yield
+        if not proc.is_alive():
+            raise RuntimeError("Worker process exited prematurely with code %d", proc.exitcode)
+    finally:
+        proc.terminate()
+        proc.join()
--- tests/system/test_system.py
+++ tests/system/test_system.py
@@ -15,24 +15,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from contextlib import contextmanager
-from threading import Thread
+from majortomo import Client
 
-from majortomo import Client, Worker, WorkerRequestsIterator
-from majortomo.broker import Broker
-
-DEFAULT_BROKER_URL = 'inproc://mdp-tests'
-HEARTBEAT_INTERVAL = 1.0
-HEARTBEAT_TIMEOUT = 3.0
+from .conftest import run_broker, run_worker
 
 
-def test_full_system_messages_delivered():
+def test_full_system_messages_delivered(socket_url):
     """Test that the entire system with one worker and client, and verify that all messages are
     delivered in order
     """
     messages = [str(i).encode('ascii') for i in range(1000)]
     replies = []
-    with broker_thread(), worker_thread(), Client(broker_url=DEFAULT_BROKER_URL) as client:
+    with run_broker(socket_url), run_worker(socket_url), Client(broker_url=socket_url) as client:
         for msg in messages:
             client.send(b'my-service', msg)
             replies.append(client.recv_all_as_list(timeout=1.0)[0])
@@ -40,12 +34,12 @@ def test_full_system_messages_delivered():
     assert replies == messages
 
 
-def test_full_system_messages_delivered_multiframe():
+def test_full_system_messages_delivered_multiframe(socket_url):
     """Verify multi-frame messages are properly delivered
     """
     messages = [[str(i * j).encode('ascii') for j in range(5)] for i in range(50)]
     replies = []
-    with broker_thread(), worker_thread(), Client(broker_url=DEFAULT_BROKER_URL) as client:
+    with run_broker(socket_url), run_worker(socket_url), Client(broker_url=socket_url) as client:
         for msg in messages:
             client.send(b'my-service', *msg)
             replies.append(client.recv_all_as_list(timeout=1.0))
@@ -53,17 +47,17 @@ def test_full_system_messages_delivered_multiframe():
     assert replies == messages
 
 
-def test_full_system_messages_delivered_multiple_workers():
+def test_full_system_messages_delivered_multiple_workers(socket_url):
     """Verify messages are properly processed in a multi-worker environment
     """
     messages = set([str(i).encode('ascii') for i in range(300)])
     replies = set()
 
-    worker1 = worker_thread()
-    worker2 = worker_thread()
-    worker3 = worker_thread()
+    worker1 = run_worker(socket_url)
+    worker2 = run_worker(socket_url)
+    worker3 = run_worker(socket_url)
 
-    with broker_thread(), Client(broker_url=DEFAULT_BROKER_URL) as client, worker1, worker2, worker3:
+    with run_broker(socket_url), Client(broker_url=socket_url) as client, worker1, worker2, worker3:
         for msg in messages:
             client.send(b'my-service', msg)
             replies.add(client.recv_all_as_list(timeout=1.0)[0])
@@ -71,7 +65,7 @@ def test_full_system_messages_delivered_multiple_workers():
     assert replies == messages
 
 
-def test_full_system_messages_delivered_multiple_workers_and_clients():
+def test_full_system_messages_delivered_multiple_workers_and_clients(socket_url):
     """Verify multi-frame messages are properly delivered
     """
     c1_messages = [str(i).encode('ascii') for i in range(30)]
@@ -80,13 +74,13 @@ def test_full_system_messages_delivered_multiple_workers_and_clients():
     c1_replies = set()
     c2_replies = set()
 
-    client1 = Client(broker_url=DEFAULT_BROKER_URL)
-    client2 = Client(broker_url=DEFAULT_BROKER_URL)
+    client1 = Client(broker_url=socket_url)
+    client2 = Client(broker_url=socket_url)
 
-    worker1 = worker_thread()
-    worker2 = worker_thread()
+    worker1 = run_worker(socket_url)
+    worker2 = run_worker(socket_url)
 
-    with broker_thread(), client1, client2, worker1, worker2:
+    with run_broker(socket_url), client1, client2, worker1, worker2:
         for i in range(30):
             client1.send(b'my-service', c1_messages[i])
             client2.send(b'my-service', c2_messages[i])
@@ -97,7 +91,7 @@ def test_full_system_messages_delivered_multiple_workers_and_clients():
     assert c2_replies == set(c2_messages)
 
 
-def test_full_system_partial_multiframe_replies():
+def test_full_system_partial_multiframe_replies(socket_url):
     """Verify multi-frame messages with partial replies are delivered properly
     """
     message_parts = [[b''.join([str(i * j).encode('ascii') for j in range(5)]) for i in range(5)] for _ in range(5)]
@@ -105,10 +99,10 @@ def test_full_system_partial_multiframe_replies():
     def handler(_):
         return (p for p in message_parts)
 
-    worker = worker_thread(handler_func=handler, send_partials=True)
+    worker = run_worker(socket_url, handler_func=handler, send_partials=True)
 
     reply = []
-    with broker_thread(), worker, Client(broker_url=DEFAULT_BROKER_URL) as client:
+    with run_broker(socket_url), worker, Client(broker_url=socket_url) as client:
         client.send(b'my-service', b'get-parts')
         for _ in message_parts[0:-1]:
             reply.append(client.recv_part(timeout=1.0))
@@ -117,7 +111,7 @@ def test_full_system_partial_multiframe_replies():
     assert message_parts == reply
 
 
-def test_full_system_partial_multiframe_replies_receive_all():
+def test_full_system_partial_multiframe_replies_receive_all(socket_url):
     """Verify multi-frame messages with partial replies are delivered properly
     """
     message_parts = [[b''.join([str(i * j).encode('ascii') for j in range(5)]) for i in range(5)] for _ in range(5)]
@@ -125,50 +119,10 @@ def test_full_system_partial_multiframe_replies_receive_all():
     def handler(_):
         return (p for p in message_parts)
 
-    worker = worker_thread(handler_func=handler, send_partials=True)
+    worker = run_worker(socket_url, handler_func=handler, send_partials=True)
 
-    with broker_thread(), worker, Client(broker_url=DEFAULT_BROKER_URL) as client:
+    with run_broker(socket_url), worker, Client(broker_url=socket_url) as client:
         client.send(b'my-service', b'get-parts')
         reply = list(client.recv_all())
 
     assert message_parts == reply
-
-
-@contextmanager
-def broker_thread(bind=DEFAULT_BROKER_URL):
-    """Run Broker in a thread, with context manager
-    """
-    broker = Broker(bind=bind, heartbeat_interval=HEARTBEAT_INTERVAL, heartbeat_timeout=HEARTBEAT_TIMEOUT)
-    thread = Thread(target=broker.run, name='BrokerThread')
-    try:
-        thread.start()
-        yield
-    finally:
-        broker.stop()
-        thread.join(timeout=3.0)
-
-
-@contextmanager
-def worker_thread(connect=DEFAULT_BROKER_URL, service_name='my-service', handler_func=lambda r: r, send_partials=False):
-    worker = Worker(broker_url=connect, service_name=service_name, heartbeat_interval=HEARTBEAT_INTERVAL,
-                    heartbeat_timeout=HEARTBEAT_TIMEOUT, zmq_linger=500)
-
-    worker_iter = WorkerRequestsIterator(worker)
-
-    def _worker_thread():
-        for request in worker_iter:
-            response = list(handler_func(request))
-            if send_partials and len(response) > 1:
-                partials = response[0:-1]
-                final = response[-1]
-                worker_iter.send_reply_from_iterable(partials, final=final)
-            else:
-                worker_iter.send_reply_final(response)
-
-    thread = Thread(target=_worker_thread, name='WorkerThread')
-    try:
-        thread.start()
-        yield worker_iter
-    finally:
-        worker_iter.stop()
-        thread.join(timeout=3.0)
